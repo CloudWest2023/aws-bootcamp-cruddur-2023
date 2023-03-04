ce5217ebd9b99e850a2e91d074ee1a151e093271
Mariachi in and out of a jar <mariachiinajar@gmail.com>
2023-03-04 18:10:28 +0000

    Added and configured Amplify

--- frontend-react-js/README.md
+++ frontend-react-js/README.md
@@ -1,3 +1,12 @@
 ## Frontend React JS
 
-Update me!
+Week 3 Amplify
+
+You have to set these variables in the `docker-compose.yaml` file. 
+Add them under the frontend > environment.
+
+REACT_AWS_PROJECT_REGION
+REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID
+REACT_APP_AWS_COGNITO_REGION
+REACT_APP_AWS_USER_POOLS_ID
+REACT_APP_CLIENT_ID
--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -13,11 +13,29 @@ import AboutPage from './pages/AboutPage';
 import TermsOfServicePage from './pages/TermsOfServicePage';
 import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
 import React from 'react';
-import process from 'process';
+import process from 'process'; 
 import {
   createBrowserRouter,
   RouterProvider
 } from "react-router-dom";
+
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure({
+  "AWS_PROJECT_REGION": process.env.REACT_AWS_PROJECT_REGION,
+  "aws_cognito_identity_pool_id": process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID,
+  "aws_cognito_region": process.env.REACT_APP_AWS_COGNITO_REGION,
+  "aws_user_pools_id": process.env.REACT_APP_AWS_USER_POOLS_ID,
+  "aws_user_pools_web_client_id": process.env.REACT_APP_CLIENT_ID,
+  "oauth": {},
+  Auth: {
+    // We are not using an Identity Pool
+    // identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID, // REQUIRED - Amazon Cognito Identity Pool ID
+    region: process.env.REACT_AWS_PROJECT_REGION,           // REQUIRED - Amazon Cognito Region
+    userPoolId: process.env.REACT_APP_AWS_USER_POOLS_ID,         // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolWebClientId: process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID,   // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+  }
+});
 
 // Define routes (path) and map them to the actual react pages (element).
 
