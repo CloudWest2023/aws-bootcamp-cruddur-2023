47eca5dcd0d12e32503da5a89e783e66e4e25e02
-–list <mariachiinajar@gmail.com>
2023-03-17 00:16:57 +0900

    Updated learning journal

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -59,7 +59,22 @@ class HomeActivities:
           'replies_count': 2,
           'reposts_count': 0,
           'replies': [
-                        {
+        
+             {
+              'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea74',
+              'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eeg',
+              'profile_picture': '/logo_mariachi_in_a_jar_white.png',
+              'handle':  'Mariachi in a Jar',
+              'message': 'Week 04 Thursday - Local Honeycomb configuration finally succeeded! T0T)...!',
+              'commit_hash': '26008b1',
+              'commit_url': 'https://github.com/CloudWest2023/aws-bootcamp-cruddur-2023/commit/26008b1468c473fe7dba6f7d3e3548f694525fa0',
+              'image': '/week02-honeycomb-success.png',
+              'likes_count': 0,
+              'replies_count': 0,
+              'reposts_count': 0,
+              'created_at': (now - timedelta(days=2)).isoformat()
+            }, 
+            {
               'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea71',
               'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eeg',
               'profile_picture': '/logo_mariachi_in_a_jar_white.png',
@@ -67,7 +82,7 @@ class HomeActivities:
               'message': 'Week 03 Tuesday - Migrating from Gitpod to Github Codespace. Open a workspace and reconfigured docker-compose file and devcontainer.json file following along Andrew\'s Codespace Crash Course.',
               'commit_hash': '28f3332',
               'commit_url': 'https://github.com/CloudWest2023/aws-bootcamp-cruddur-2023/commit/28f33323f7e117506aa6aebc62364218559e33bd',
-              'image': '/week2-error-nospace.png',
+              'image': '/week02-error-nospace.png',
               'likes_count': 0,
               'replies_count': 0,
               'reposts_count': 0,
@@ -79,7 +94,7 @@ class HomeActivities:
               'profile_picture': '/logo_mariachi_in_a_jar_white.png',
               'handle':  'Mariachi in a Jar',
               'message': 'Week 03 Monday - Week04 Sunday - Finished instrumenting Xray!',
-              'image': '/week2-xray-daemon-working.png',
+              'image': '/week02-xray-daemon-working.png',
               'likes_count': 0,
               'replies_count': 0,
               'reposts_count': 0,
