4735341e7c844c1a464c94b1fd8b3d2057889249
Pinata in Cloud <coding.pinata@proton.me>
2023-04-09 17:48:20 +0000

    Week 4 livestream - revision complete

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,14 +2,22 @@
 
 INSERT INTO public.users (display_name, handle, cognito_user_id)
 VALUES
-    ('Mariachi in a Jar', 'mariachiinajar', 'MOCK'),
-    ('Matryoshka in the Cloud', 'matryo', 'MOCK');
+    ('Coding Vihuela', 'coding.vihuela', 'MOCK'),
+    ('Coding Pinata', 'coding.pinata', 'MOCK');
+
+    -- ('Mariachi in a Jar', 'mariachiinajar', 'MOCK'),
+    -- ('Matryoshka in the Cloud', 'matryo', 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
     (
-        (SELECT uuid FROM public.users WHERE users.handle = 'mariachiinajar' LIMIT 1),
-        'This was imported as seed data!',
+        (SELECT uuid FROM public.users WHERE users.handle = 'coding.vihuela' LIMIT 1),
+        'coding.vihuela is a clone 1 of Mariachi in a Jar',
+        current_timestamp + interval '10 day'
+    ),
+    (
+        (SELECT uuid FROM public.users WHERE users.handle = 'coding.pinata' LIMIT 1),
+        'coding.pinata is an alternate developer 2 of Mariachi in a Jar',
         current_timestamp + interval '10 day'
     )
     
