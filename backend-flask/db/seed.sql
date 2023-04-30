706bf9d7075e87ec9a6044f907dd30baef392e48
A friend of cloudy engineer Mariachi <coding.vihuela@proton.me>
2023-04-30 17:20:57 +0000

    Debug - db.query_value returns None type. Seeded correct data.

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,22 +2,24 @@
 
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-    ('Coding Vihuela', 'coding.vihuela', 'viuela@gmail.com', 'MOCK'),
-    ('Coding Pinata', 'coding.pinata', 'pinata@gmail.com', 'MOCK');
+    -- ('Coding Vihuela', 'coding.vihuela', 'viuela@gmail.com', 'MOCK'),
+    -- ('Coding Pinata', 'coding.pinata', 'pinata@gmail.com', 'MOCK');
 
     -- ('Mariachi in a Jar', 'mariachiinajar', 'MOCK'),
     -- ('Matryoshka in the Cloud', 'matryo', 'MOCK');
+
+    ('Billy Paigh', 'billypaigh', 'billypaigh@gmail.com', 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
     (
         (SELECT uuid FROM public.users WHERE users.handle = 'coding.vihuela' LIMIT 1),
-        'Is the db on AWS Cloud connected?',
+        'Currently fighting through Week 5!',
         current_timestamp + interval '10 day'
     ),
     (
         (SELECT uuid FROM public.users WHERE users.handle = 'coding.pinata' LIMIT 1),
-        'If you are seeing this message, then yes, it is connected!',
+        'I came along way to fight "Week 5 Implement Conversations with DynamoDB"!!',
         current_timestamp + interval '10 day'
     )
     
