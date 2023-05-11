9d27eef37393588ef14f72dc3eb20d8b6ed09fc6
Mariachi in and out of a jar <mariachiinajar@gmail.com>
2023-05-11 14:52:35 +0000

    DEBUG - Turned of honeycomb

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -35,13 +35,13 @@ from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 ####################### HONEYCOMB #######################
 # OTEL packages  --------------------------
-from opentelemetry import trace
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor   # this is middleware
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+# from opentelemetry import trace
+# from opentelemetry.instrumentation.flask import FlaskInstrumentor
+# from opentelemetry.instrumentation.requests import RequestsInstrumentor   # this is middleware
+# from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+# from opentelemetry.sdk.trace import TracerProvider
+# from opentelemetry.sdk.trace.export import BatchSpanProcessor
+# from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
 ####################### ROLLBAR #######################
 import rollbar
@@ -54,17 +54,17 @@ from flask import got_request_exception
 
 ####################### HONEYCOMB #######################
 # Initialize HONEYCOMB tracing and an exporter that can send dta to Honeycomb
-provider = TracerProvider()
+# provider = TracerProvider()
 
-processor = BatchSpanProcessor(OTLPSpanExporter()) # Reads the env variables for the configuration of where to send the spans.
-provider.add_span_processor(processor)
+# processor = BatchSpanProcessor(OTLPSpanExporter()) # Reads the env variables for the configuration of where to send the spans.
+# provider.add_span_processor(processor)
 
-# Show this in the logs within the backend-flask app )STDOUT)
-# simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
-# provider.add_span_processor(simple_processor)
+# # Show this in the logs within the backend-flask app )STDOUT)
+# # simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+# # provider.add_span_processor(simple_processor)
 
-trace.set_tracer_provider(provider)
-tracer = trace.get_tracer(__name__)
+# trace.set_tracer_provider(provider)
+# tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
 jwttv = JWTTokenVerifier(
@@ -81,8 +81,8 @@ app.config['AWS_COGNITO_USER_POOL_ID'] = os.getenv('AWS_COGNITO_USER_POOL_ID')
 
 ####################### HONEYCOMB #######################
 # Initialize automatic instrumentation with Flask
-FlaskInstrumentor().instrument_app(app)
-RequestsInstrumentor().instrument()
+# FlaskInstrumentor().instrument_app(app)
+# RequestsInstrumentor().instrument()
 
 
 
